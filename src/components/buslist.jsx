a3b48c22a847849960250b5708462d157afe8f15
muthapavani <muttapavani9@gmail.com>
2025-09-22 23:10:35 +0530

    Add files via upload

--- src/components/buslist.jsx
+++ src/components/buslist.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "./Buslist.css";
+import "./buslist.css";
 
 const Buslist = () => {
   const [buses, setBuses] = useState([]);
